0ac9ed559f7ea302b6f7a76efb94885fe126ebff
arvndblocker"nn <aravindvasang@gmail.com>
2023-11-10 12:50:06 +0530

    front-end

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Hero, Breadcrumbs } from "@/components/common";
+import { CourseList } from "@/components/course";
+import { BaseLayout } from "@/components/layout";
+import { OrderCard } from "@/components/order";
+import { EthRates, WalletBar } from "@/components/web3";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BaseLayout>
+      <Hero />
+      <Breadcrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </BaseLayout>
+  );
 }
